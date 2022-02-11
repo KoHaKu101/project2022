79b3f314d501f07804e88e4c5e6d8339ee04667d
unknown <poou8558@gmail.com>
2022-02-11 21:01:49 +0700

    'backsetting'

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -9,10 +9,10 @@ class HomepageController extends Controller
 {
   public function homepage()
     {
-        $DATA_SLIDE = Settingnumber::where('TYPE_SETTING', '=', 'SLIDE')->first();
-        $IMG_SLIDE = isset($DATA_SLIDE->TYPE_NUMBER) ? Img::where("UNID_SETTING_NUMBER",'=',$DATA_SLIDE->UNID )->where("STATUS",'=',"OPEN")->get() : false;
-        $IMG_DIRECTOR = Img::where('IMG_TYPE','=','DIRECTOR')->first();
-        $LIMIT_NUMBER = isset($DATA_SLIDE->TYPE_NUMBER) ? count($IMG_SLIDE) : '5';
+        $DATA_SLIDE     = Settingnumber::where('TYPE_SETTING', '=', 'SLIDE')->first();
+        $IMG_SLIDE      = isset($DATA_SLIDE->TYPE_NUMBER) ? Img::where("UNID_SETTING_NUMBER",'=',$DATA_SLIDE->UNID )->where("STATUS",'=',"OPEN")->get() : false;
+        $IMG_DIRECTOR   = Img::where('IMG_TYPE','=','DIRECTOR')->first();
+        $LIMIT_NUMBER   = isset($DATA_SLIDE->TYPE_NUMBER) ? count($IMG_SLIDE) : 5;
 
         return view('homepage',compact('LIMIT_NUMBER','IMG_SLIDE','IMG_DIRECTOR'));
     }}
