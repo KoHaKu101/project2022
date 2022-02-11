825a6c40d0611db669200e8627ba800604b62ef9
unknown <poou8558@gmail.com>
2022-02-11 21:33:15 +0700

    'backsetting'

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -12,6 +12,7 @@ class HomepageController extends Controller
         $DATA_SLIDE     = Settingnumber::where('TYPE_SETTING', '=', 'SLIDE')->get();
         $IMG_SLIDE      = count($DATA_SLIDE) > 0 ? Img::where("IMG_TYPE",'=','SLIDE')->where("STATUS",'=',"OPEN")->get() : false;
         $IMG_DIRECTOR   = Img::where('IMG_TYPE','=','DIRECTOR')->first();
-        return view('homepage',compact('DATA_SLIDE','IMG_SLIDE','IMG_DIRECTOR'));
+        $COUNT_SLIDE    = count($DATA_SLIDE) > 0 ? count($DATA_SLIDE) : 0;
+        return view('homepage',compact('COUNT_SLIDE','IMG_SLIDE','IMG_DIRECTOR'));
     }
 }
